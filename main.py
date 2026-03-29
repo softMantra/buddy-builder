ba3719eb94939d1e4da58b5c99851ae74ae8bf6c
roser <solutionssoft847@gmail.com>
2026-03-30 05:14:09 +0530

    Fix TemplateResponse parameters in read_root

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @app.get("/editor", response_class=HTMLResponse)
 async def editor_ui():
